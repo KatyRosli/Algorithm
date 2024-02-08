9e4e8a94ec5479d611872483bff1c2cd27ee851f
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-08 13:44:43 +0100

    quicksort-divide and conquer strategy

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,32 @@
-export default function quick_sort(arr: number[]): void {
+//this is divide and conquer strategy
 
+function quickSort(array: number[], low: number, high: number): void{
+  if(low >= high) {
+    return;
+  }
+  const pivotIdx = partition(array, low, high);
+  quickSort(array, low, pivotIdx -1);
+  quickSort(array, pivotIdx + 1, high);
+}
+
+function partition(array: number[], low: number, high: number): number {
+  const pivot = array[high];
+  let idx = low - 1;
+
+  for(let i = low; i < high; i++) {
+    if(array[i] <= pivot) {
+      idx++;
+      const tmp = array[i];
+      array[i] = array[idx];
+      array[idx] = tmp;
+    }
+  }
+  idx++;
+  array[high] = array[idx];
+  array[idx] = pivot;
+  return idx;
+}
+
+export default function quick_sort(array: number[]): void {
+  quickSort(array, 0, array.length -1);
 }
