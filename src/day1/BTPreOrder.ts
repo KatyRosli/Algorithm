5be7bd5bf778d195f1e6b6a7492473fc2aa935da
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-09 13:37:16 +0100

    BTPreOrder

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,21 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(current: BinaryNode<number> | null, path: number[]): number[]{
+  if(!current){
+    return path;
+  }
 
+  //recurse
+  //pre
+  path.push(current.value);
+
+  //recurse
+  walk(current.left, path);
+  walk(current.right, path);
+
+  //post
+  return path;
+  
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
