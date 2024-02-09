4400f4d31d575a3683195ec7902231101c5569ec
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-09 13:43:24 +0100

    BTInOrder and BTPostOrder

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,22 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(current: BinaryNode<number> | null, path: number[]): number[]{
+  if(!current){
+    return path;
+  }
 
+  //recurse
+  //pre
+  
+
+  //recurse
+  walk(current.left, path);
+  path.push(current.value);
+  walk(current.right, path);
+
+  //post
+  return path;
+  
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,22 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(current: BinaryNode<number> | null, path: number[]): number[]{
+  if(!current){
+    return path;
+  }
 
+  //recurse
+  //pre
+  
+
+  //recurse
+  walk(current.left, path);
+  walk(current.right, path);
+  path.push(current.value);
+
+  //post
+  return path;
+  
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
