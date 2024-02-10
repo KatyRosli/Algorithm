747a6b15cc61ef4055ce4f1b5f19efdba230e215
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-10 20:17:56 +0100

    solve BinaryTreeBreathFirstSearch

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+//Binary Tree Breath First Search
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<number> | null)[] = [head];
+
+  while(q.length) {
+    const current = q.shift() as BinaryNode<number> | undefined | null;
+    if(!current){
+      continue;
+    }
+    //search
+    if(current?.value === needle) {
+      return true;
+    }
+    
+      q.push(current.left);
+      q.push(current.right);
+    
+    
+  }
+  return false;
 }
