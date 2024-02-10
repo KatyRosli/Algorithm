b142684f67484aebbe1f59082d2c855c84bfea28
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-10 20:30:21 +0100

    CompareBinaryTree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,16 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
-
+  //base case
+  if(a === null && b === null){
+    return true;
+  }
+  //next base case
+  if(a === null || b === null){
+    return false;
+  }
+  //last base case
+  if(a.value !== b.value){
+    return false;
+  }
+  //for entire tree now
+  return compare(a.left, b.left) && compare(a.right, b.right);
 }
