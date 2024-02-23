caa09049d34875ccbcc1d4d538543fa86c2e6fdb
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-23 16:00:51 +0100

    fix dfsGraphList

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,50 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+  graph: WeightedAdjacencyList,
+  current: number,
+  needle: number,
+  seen: boolean[],
+  path: number[]
+): boolean {
+  if (current === needle) {
+    path.push(current); // Include the needle in the path
+    return true;
+  }
 
+  if (seen[current]) {
+    return false;
+  }
+
+  seen[current] = true;
+
+  // Recurse
+  path.push(current);
+
+  const list = graph[current];
+  for (let i = 0; i < list.length; i++) {
+    const edge = list[i];
+
+    if (walk(graph, edge.to, needle, seen, path)) {
+      return true;
+    }
+  }
+
+  // If the current node doesn't lead to the needle, backtrack
+  path.pop();
+
+  return false;
+}
+
+export default function dfs(
+  graph: WeightedAdjacencyList,
+  source: number,
+  needle: number
+): number[] | null {
+  const seen: boolean[] = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  if (!walk(graph, source, needle, seen, path)) {
+    return null;
+  }
+
+  return path;
 }
