75edb6ae8e5f2eab8772c9c2e2e85d998036b2ef
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2024-02-23 15:45:30 +0100

    fix bfsgraphmatrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,44 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+  graph: WeightedAdjacencyMatrix, 
+  source: number, 
+  needle: number
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+    
+    seen[source] = true;
+    const q: number[] = [source];
 
+    while (q.length) {
+      const current = q.shift() as number;
+      if (current === needle) {
+        break;
+      }
+      const adjs = graph[current];
+      for (let i = 0; i < graph.length; i++) {
+        if (adjs[i] === 0) {
+          continue;
+        }
+        if (seen[i]) {
+          continue;
+        }
+        seen[i] = true;
+        prev[i] = current;
+        q.push(i);
+      }
+    }
+    
+    if (prev[needle] === -1) {
+      return null;
+    }
+
+    // Build the path backwards
+    let current = needle;
+    const out: number[] = [];
+    while (prev[current] !== -1) {
+      out.push(current);
+      current = prev[current];
+    }
+    out.push(source); // Add the source node
+    return out.reverse();
 }
